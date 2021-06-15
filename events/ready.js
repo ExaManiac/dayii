9428d03a395bec9dcba40d8598c7a087a033a392
ExaManiac <68477122+ExaManiac@users.noreply.github.com>
2021-06-15 13:19:09 +0400

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+module.exports = async (client, user,message)  => {
+  client.user.setActivity("gelişmiş ayarlanabilir emoji rol sistemi uWu")
+  console.log("Bot Başarıyla Başlatıldı!")
+}
+
+// youtube.com/c/EmirhanSarac
